a4cbdb03878f7c2d57add33a27ab6e1b0ccddb53
Randall Pruim <rpruim@gmail.com>
2018-08-14 08:37:26 -0400

    add reprex()

---
+++ R/reprex.R
@@ -0,0 +1,27 @@
+
+#' Reproducible Examples
+#'
+#' Create a reprex with default figure size set and easily adjusted.
+#'
+#' @inheritParams reprex::reprex
+#' @param ... Additional arguments passed on to [`reprex::reprex()`].
+#' @importFrom reprex reprex
+#' @importFrom utils modifyList
+#' @export
+#' @seealso [`reprex::reprex()`]
+
+reprex <-
+  function(
+    x, ...,
+    fig.width = 6, fig.height = 3,
+    opts_chunk = list()) {
+
+    reprex::reprex(
+      x, ...,
+      opts_chunk =
+        utils::modifyList(
+          list(fig.width = fig.width, fig.height = fig.height),
+          opts_chunk
+        )
+    )
+}
